1f1cf9277e559abc44ba4bacca2c98805d4d8121
yogesh1122 <Samrat1903y@gmail.com>
2023-03-11 00:41:15 +0530

    addPostManCollection

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,8 @@ const { fetchFakeAPI, getRandomNumber } = require('../service/service');
 
 //user Api's
 async function getCityWise(req,res) {
-   const { } = req.query;
+ try {
+       const { } = req.query;
    let temp = []
    await (await fakeAPIModel.find({},{name:1,email:1,dob:1,"location.city":1}).skip(0).limit(5)).map(function(doc) {
     var fullName = doc.name.title + " " + doc.name.first + " " + doc.name.last;
@@ -18,6 +19,9 @@ async function getCityWise(req,res) {
   })
 
    res.status(200).send({count:temp.length,temp}) 
+ } catch (error) {
+    
+ }
 }
 
 async function updateMany(req,res) {
@@ -37,19 +41,26 @@ async function updateMany(req,res) {
         res.status(200).send({Msg:'all record updated',saveSal:saveSal.matchedCount})
     
     } catch (error) {
-        console.log(error);
+    res.status(500).send({msg:'Internal server error',error})
     }
 }
 //SEED API'S
 async function  getUserData(req,res){
+    try {
+        
+        const ax = await fetchFakeAPI() //call 3 apis
+        // console.log(ax.data.results[0]);
+        const  savefkdata = await fakeAPIModel(ax.data.results[0]).save();  //save own db
+        res.status(200).send({data:savefkdata})
 
-    const ax = await fetchFakeAPI() //call 3 apis
-    // console.log(ax.data.results[0]);
-    const  savefkdata = await fakeAPIModel(ax.data.results[0]).save();  //save own db
-    res.status(200).send({data:savefkdata})
+    } catch (error) {
+    res.status(500).send({msg:'Internal server error',error})
+        
+    }
 } 
 
 async function fakeDataSeed(req,res) {
+  try {
     const { rid } = req.query;
     const recordArr =[]
     if(!rid) return res.status(400).send({msg:"record rid should be present"})
@@ -63,6 +74,10 @@ async function fakeDataSeed(req,res) {
 
     res.status(200).send({msg:`${recordArr.length} records save successfully`,recordArr})
 
+    
+   } catch (error) {
+    res.status(500).send({msg:'Internal server error',error})
+   }
 
 }
 
